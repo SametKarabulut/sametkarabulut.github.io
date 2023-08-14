ce71664dff8f508e51e724180aa43804b6d39967
SametKarabulut <142041222+SametKarabulut@users.noreply.github.com>
2023-08-14 23:46:22 +0300

    update1

--- anm.js
+++ anm.js
@@ -4,10 +4,10 @@ var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
 
 $(ele).hide().appendTo('p').each(function (i) {
-    $(this).delay(250 * i).css({
+    $(this).delay(150 * i).css({
         display: 'inline',
         opacity: 0
     }).animate({
         opacity: 1
-    }, 250);
+    }, 150);
 });
