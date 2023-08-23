1c6f8739c5d4952a1625ff8643ae51cd9b78739c
SametKarabulut <142041222+SametKarabulut@users.noreply.github.com>
2023-08-23 23:07:01 +0300

    update

--- Challanges/5/main.js
+++ Challanges/5/main.js
@@ -1,8 +1,27 @@
-const slides = document.querySelectorAll('.slide');
+const apiUrl = "https://raw.githubusercontent.com/cuneydbolukoglu/challenge/main/challenge-carousel.json";
+const slides = document.querySelector('.slide img');
 const prevButton = document.querySelector('.slider-prev');
 const nextButton = document.querySelector('.slider-next');
 const indicators = document.querySelectorAll('.indicator');
 let currentSlide = 0;
+
+axios.get(`${apiUrl}`).then(
+    response =>{
+        const respdata = response.data;
+        respdata.map(item=>{
+            const img = document.createElement('img');
+             img.src = item.imgUrl;
+            slides.appendChild(img);
+            const slidesimg = document.querySelector('img');
+            /*slidesimg.setAttribute("class","closedslider");*/
+            /*slidesimg.forEach(slide => slide.style.display = 'none');*/
+        })
+    }, 
+    error => {
+        console.log('Error : ' , error);
+    }
+);
+
 
 function updateIndicators(index){
     indicators.forEach(indicator => indicator.classList.remove('active'));
@@ -18,6 +37,7 @@ function prevSlide() {
   currentSlide--;
   if (currentSlide < 0) {
     currentSlide = slides.length - 1;
+
   }
   showSlide(currentSlide);
   updateIndicators(currentSlide);
@@ -34,3 +54,5 @@ function nextSlide() {
 
 prevButton.addEventListener('click', prevSlide);
 nextButton.addEventListener('click', nextSlide);
+
+
