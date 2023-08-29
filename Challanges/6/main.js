c717f91dae8379aae5ecd1d26487074bf7895e31
SametKarabulut <142041222+SametKarabulut@users.noreply.github.com>
2023-08-30 00:28:27 +0300

    jsupdate

--- Challanges/6/main.js
+++ Challanges/6/main.js
@@ -7,51 +7,83 @@ const maleRadio = document.querySelector('input[value="erkek"]');
 const femaleRadio = document.querySelector('input[value="kadin"]');
 const jobInput= document.getElementById('meslek');
 const jobInputValue = jobInput.value;
+const messageTextarea= document.getElementById('mesaj');
 
-SendButton.addEventListener('click', () => {
 
-    if (adSoyadInput.value.length === 0) {
-        console.log("Ad Soyad alanını boş bıraktınız !");
-    } else {
-        console.log(adSoyadInput.value);
-    };
-   
-    if (mailInput.value.length === 0) {
-        console.log("Mail Alanını boş bıraktınız !");
-    } if (mailInput.value.indexOf('@') === -1) {
-        console.log("Geçerli bir email adresi giriniz.");
-    } else {
-        console.log(mailInput.value)
-    };
 
-    if (phoneInput.value.length === 0) {
-        console.log("Telefon alanını Boş Bıraktınız !");
-    } if (phoneInput.value.length < 10 || phoneInput.value.length > 10) {
-        console.log("Telefon numarası 10 karakter olmalıdır.");
-    } else {
-        console.log(phoneInput.value);
-    };
-    
-    if (subjectInput.value.length === 0) {
-        console.log("Konu alanını boş bıraktınız !");
-    } else {
-        console.log(subjectInput.value);
-    };
 
-    if (maleRadio.checked) {
-        console.log("Erkek seçildi.");
-      } else if (femaleRadio.checked) {
-        console.log("Kadın seçildi.");
+const showError = (inputElement, message) => {
+    inputElement.style.border = '2px solid red';
+    inputElement.style.opacity = '1';
+    console.log(message);
+  };
+  
+  const showSuccess = (inputElement, value) => {
+    inputElement.style.border = '';
+    inputElement.style.opacity = '';
+    console.log(value);
+  };
+  
+
+    const validateForm = () => {
+    if (adSoyadInput.value === '') {
+        showError(adSoyadInput, 'Ad Soyad alanını boş bıraktınız !');
       } else {
-        console.log("Cinsiyet seçimi yapınız.");
-      };
-
-      const jobInputValue = jobInput.value;
-      if (jobInputValue === 'BOS') {
-        console.log("Meslek şeçimi yapınız !")
-      }else {
-        console.log(`Seçilen değer: ${jobInputValue}`);
+        showSuccess(adSoyadInput, `Ad Soyad: ${adSoyadInput.value}`);
+      }
+   
+      if (mailInput.value === '') {
+        showError(mailInput, 'Mail Alanını boş bıraktınız !');
+      } else if (mailInput.value.indexOf('@') === -1) {
+        showError(mailInput, 'Geçerli bir email adresi giriniz.');
+      } else {
+        showSuccess(mailInput, `Mail Adresi: ${mailInput.value}`);
       }
 
-  });
 
+      if (phoneInput.value === '') {
+        showError(phoneInput, 'Telefon alanını Boş Bıraktınız !');
+      } else if (phoneInput.value.length !== 10) {
+        showError(phoneInput, 'Telefon numarası 10 karakter olmalıdır.');
+      } else {
+        showSuccess(phoneInput, `Telefon Numarası: ${phoneInput.value}`);
+      }
+    
+
+      if (subjectInput.value === '') {
+        showError(subjectInput, 'Konu alanını boş bıraktınız !');
+      } else {
+        showSuccess(subjectInput,  `Konu: ${subjectInput.value}`);
+      };
+      
+      if (maleRadio.checked) {
+        showSuccess(maleRadio, 'Cinsiyet: Erkek');
+        femaleRadio.style.border = '';
+        femaleRadio.style.opacity = '';
+      } else if (femaleRadio.checked) {
+        showSuccess(femaleRadio, 'Cinsiyet: Kadın');
+        maleRadio.style.border = '';
+        maleRadio.style.opacity = '';
+      } else {
+        showError(maleRadio, 'Cinsiyet seçimi yapınız.');
+        femaleRadio.style.border = '2px solid red';
+        femaleRadio.style.opacity = "1";
+      }
+
+
+if (jobInput.value === 'BOS') {
+    showError(jobInput, 'Meslek seçimi yapınız !');
+  } else {
+    showSuccess(jobInput, `Meslek: ${jobInput.value}`);
+  }
+
+  if (messageTextarea.value === '') {
+    showError(messageTextarea, 'Mesaj alanını boş bıraktınız !');
+  } else {
+    showSuccess(messageTextarea, `Mesaj: ${messageTextarea.value}`);
+  }
+
+};
+
+
+SendButton.addEventListener('click', validateForm);
